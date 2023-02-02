0ff931628bdb835e44a51f49687a1bd7b67129a5
Henrique Ranel <henrique.ranel@outlook.com>
2023-02-01 21:07:04 -0300

    add endpoint de status

--- src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
@@ -12,5 +16,13 @@ public class HealthCheckController {
     @GetMapping
     public ResponseEntity<Object> status(){
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("version")
+    public ResponseEntity<String> version() throws IOException {
+        Properties prop = new Properties();
+        InputStream imput = getClass().getClassLoader().getResourceAsStream("application.yml");
+        prop.load(imput);
+        return ResponseEntity.ok(prop.getProperty("build.name") + ": " + prop.getProperty("build.version"));
     }
 }
