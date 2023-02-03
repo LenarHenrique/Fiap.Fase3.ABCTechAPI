f36d3a6172f18f3a59f2942a0bc7b09c3be0bca4
Gustavo Aveiro <gustavo.aveiro03@gmail.com>
2023-02-02 22:13:23 -0300

    Implementação de testes unitários para a classe HealthCheckController

--- src/test/java/br/com/fiap/abctechapi/AbctechapiApplicationTests.java
+++ src/test/java/br/com/fiap/abctechapi/AbctechapiApplicationTests.java
@@ -1,10 +1,12 @@
 package br.com.fiap.abctechapi;
 
+import br.com.fiap.abctechapi.controller.HealthCheckController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AbctechapiApplicationTests {
+
 
 	@Test
 	void contextLoads() {
