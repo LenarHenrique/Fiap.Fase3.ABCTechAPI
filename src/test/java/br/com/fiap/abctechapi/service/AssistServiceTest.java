09c0663693f39c6470ce3a48ba451aee5eaa2d61
Henrique Ranel <henrique.ranel@outlook.com>
2023-02-06 20:59:00 -0300

    Ajustes nos testes
    - Remove teste de controller e component pois é necessario Interface para funcionar

--- src/test/java/br/com/fiap/abctechapi/service/AssistServiceTest.java
+++ src/test/java/br/com/fiap/abctechapi/service/AssistServiceTest.java
@@ -4,10 +4,10 @@ package br.com.fiap.abctechapi.service;
 import br.com.fiap.abctechapi.model.Assist;
 import br.com.fiap.abctechapi.repository.AssistRepository;
 import br.com.fiap.abctechapi.service.impl.AssistServiceImpl;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,16 +24,16 @@ public class AssistServiceTest {
     private AssistService assistService;
 
     @BeforeEach
-    public void init(){
+    void init() {
         MockitoAnnotations.openMocks(this);
         assistService = new AssistServiceImpl(assistRepository);
     }
 
-    @DisplayName("Teste Lista Sucesso com 2 valores")
+    @DisplayName("Testando retorno lista com sucesso com 2 itens")
     @Test
-    public void list_success(){
-        Assist assist = new Assist(1L, "teste","desc");
-        Assist assist2 = new Assist(2L, "teste 2","desc 2");
+    void list_success(){
+        Assist assist = new Assist(1L, "Test", "Description Test");
+        Assist assist2 = new Assist(2L, "Test 2", "Description Test 2");
 
         when(assistRepository.findAll()).thenReturn(List.of(assist, assist2));
 
@@ -44,7 +44,7 @@ public class AssistServiceTest {
         Assertions.assertEquals(values.get(1).getId(), assist2.getId());
     }
 
-    @DisplayName("Teste de Lista vazia")
+    @DisplayName("Testando retorno lista vazia")
     @Test
     void empty_list(){
         when(assistRepository.findAll()).thenReturn(List.of());
--- src/test/java/br/com/fiap/abctechapi/service/OrderServiceTest.java
+++ src/test/java/br/com/fiap/abctechapi/service/OrderServiceTest.java
@@ -6,32 +6,36 @@ import br.com.fiap.abctechapi.model.Assist;
 import br.com.fiap.abctechapi.model.Order;
 import br.com.fiap.abctechapi.repository.AssistRepository;
 import br.com.fiap.abctechapi.repository.OrderRepository;
+import br.com.fiap.abctechapi.service.IOrderService;
 import br.com.fiap.abctechapi.service.impl.OrderService;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static  org.mockito.Mockito.*;
+import static org.mockito.Mockito.*;
 
 public class OrderServiceTest {
 
     @Mock
     private OrderRepository orderRepository;
+
     @Mock
     private AssistRepository assistRepository;
-    private OrderService orderService;
+
+    private IOrderService orderService;
 
     @BeforeEach
     public void init(){
         MockitoAnnotations.openMocks(this);
-        this.orderService = new OrderService(orderRepository, assistRepository);
-        when(assistRepository.findById(any()))
-                .thenReturn(Optional.of(new Assist(1L, "TESTE", "Desc")));
+        orderService = new OrderService(orderRepository, assistRepository);
+        when(assistRepository.findById(any())).
+                thenReturn(Optional.of(new Assist(1L, "Teste", "Description Test")));
     }
 
     @Test
@@ -40,7 +44,7 @@ public class OrderServiceTest {
     }
 
     @Test
-    public  void create_order_success() throws Exception{
+    public void create_order_success() throws Exception{
         Order newOrder = new Order();
         newOrder.setOperatorId(1234L);
 
@@ -50,21 +54,21 @@ public class OrderServiceTest {
     }
 
     @Test
-    public void create_order_erro_minimum()throws Exception{
+    public void create_order_error_minimum() throws Exception {
         Order newOrder = new Order();
         newOrder.setOperatorId(1234L);
 
         Assertions.assertThrows(MinAssistsException.class, () -> orderService.saveOrder(newOrder, List.of()));
-
         verify(orderRepository, times(0)).save(newOrder);
+
     }
+
     @Test
-    public void create_order_erro_maximum()throws Exception{
+    public void create_order_error_maximum() throws Exception {
         Order newOrder = new Order();
         newOrder.setOperatorId(1234L);
 
-        Assertions.assertThrows(MaxAssistsException.class, () -> orderService.saveOrder(newOrder, List.of(1L,2L,3L,4L,5L,6L,1L,2L,3L,4L,5L,6L,1L,2L,3L,4L)));
-
+        Assertions.assertThrows(MaxAssistsException.class, () -> orderService.saveOrder(newOrder, List.of(1L,2L,3L,4L,5L,6L,7L,8L,9L,10L,11L,12L,13L,14L,15L,16L)));
         verify(orderRepository, times(0)).save(newOrder);
     }
 
